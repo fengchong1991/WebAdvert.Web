849e91c7b6733451f235a3feb7f3cb3e119a324a
Chong Feng <chong.feng@koresoftware.com>
2019-07-25 17:20:10 +1000

    -

--- WebAdvert.Api/WebAdvert.Api/WebAdvert.Api/Controllers/HomeController.cs
+++ WebAdvert.Api/WebAdvert.Api/WebAdvert.Api/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using WebAdvert.Api.Models;
 using WebAdvert.Api.Services;
+using WebAdvert.Api.Services.Dynamodb;
 
 namespace WebAdvert.Api.Controllers
 {
